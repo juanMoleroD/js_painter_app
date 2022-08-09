313aed9c34dd98120d398bdcdd4f487396948344
juanMoleroD <juanldimeco@gmail.com>
2022-08-09 16:32:53 +0100

    added test for decorator

--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,2 +1,16 @@
 const assert = require('assert');
-const decorator = 
+const decorator = require("../decorator");
+
+describe("decorator", function () {
+    let decorator;
+    this.beforeEach(function() {
+        decorator = new decorator();
+    })
+
+    it("should start with an empty stock", function () {
+        const actual = decorator.stock;
+        assert.strictEqual(actual, []);
+    })
+
+
+});
